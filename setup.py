77eb496e3323d2bb64c7aeb123e399cfda56a3e9
krishvsoni <sonikrish994@gmail.com>
2024-02-11 20:09:53 +0530

    wiz-commit

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='flask-wiz',
-    version='1.2',
+    version='1.2.1',
     author='Krish Soni',
     packages=find_packages(),
     include_package_data=True,
@@ -15,4 +18,6 @@ setup(
             'flask-wiz=flask_wiz.cli:cli',
         ],
     },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
