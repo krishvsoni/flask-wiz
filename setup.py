747a174fa73810a55e6b6cdb2e871f551977601d
krishvsoni <sonikrish994@gmail.com>
2024-02-12 16:02:42 +0530

    Update 1.4

--- setup.py
+++ setup.py
@@ -3,16 +3,36 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+selected_database = ['sqlite','mysql-connector-python',
+    'psycopg2',
+    'pymongo'  ]
+
+dependencies = [
+    'flask',
+    'click',
+    'sqlite',
+    'mysql-connector-python',
+    'psycopg2',
+    'pymongo',
+
+]
+
+if selected_database == 'sqlite':
+    dependencies.append('sqlite')
+elif selected_database == 'mysql':
+    dependencies.append('mysql-connector-python')
+elif selected_database == 'postgresql':
+    dependencies.append('psycopg2')
+elif selected_database == 'mongodb':
+    dependencies.append('pymongo')
+
 setup(
     name='flask-wiz',
-    version='1.2.1',
+    version='1.4',
     author='Krish Soni',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'flask',
-        'click',
-    ],
+    install_requires=dependencies,
     entry_points={
         'console_scripts': [
             'flask-wiz=flask_wiz.cli:cli',
