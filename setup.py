2cb7c1f58223f7d96ba210f8c9a1d6943efcd547
krishvsoni <sonikrish994@gmail.com>
2024-02-13 15:56:14 +0530

    1.6.3

--- setup.py
+++ setup.py
@@ -3,36 +3,23 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# selected_database = ['sqlite','mysql-connector-python',
-#     'psycopg2',
-#     'pymongo'  ]
-
-# dependencies = [
-#     'flask',
-#     'click',
-#     'sqlite',
-#     'mysql-connector-python',
-#     'psycopg2',
-#     'pymongo',
-
-# ]
-
-# if selected_database == 'sqlite':
-#     dependencies.append('sqlite')
-# elif selected_database == 'mysql':
-#     dependencies.append('mysql-connector-python')
-# elif selected_database == 'postgresql':
-#     dependencies.append('psycopg2')
-# elif selected_database == 'mongodb':
-#     dependencies.append('pymongo')
+dependencies = [
+    'flask',
+    'click',
+    'Flask',  
+    'sqlite',
+    'mysql-connector-python',
+    'psycopg2',
+    'pymongo',
+]
 
 setup(
     name='flask-wiz',
-    version='1.5.1',
+    version='1.6.3',
     author='Krish Soni',
     packages=find_packages(),
     include_package_data=True,
-   # install_requires=dependencies,
+    install_requires=dependencies,  
     entry_points={
         'console_scripts': [
             'flask-wiz=flask_wiz.cli:cli',
